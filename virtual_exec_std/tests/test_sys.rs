0d73725b1418ba61b7e22a00bf1eeba4241151b6
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2026-05-01 00:35:48 +0100

    Release 0.5.0-alpha.1
    virtual_exec@0.5.0-alpha.1
    virtual_exec_core@0.5.0-alpha.1
    virtual_exec_extern@0.5.0-alpha.1
    virtual_exec_macro@0.5.0-alpha.1
    virtual_exec_parser@0.5.0-alpha.1
    virtual_exec_std@0.5.0-alpha.1
    virtual_exec_type@0.5.0-alpha.1
    
    Generated by cargo-workspaces

---
+++ virtual_exec_std/tests/test_sys.rs
@@ -0,0 +1,21 @@
+use virtual_exec_core::{Machine, parse, compile};
+use virtual_exec_core::sequential::exec::State;
+use virtual_exec_std::{BASIC, SYS};
+
+#[test]
+fn test_print() {
+    let code = "a = \"test\n\"; print(a);";
+    let compiled = compile(&parse(code).unwrap());
+    println!("{:?}", compiled);
+    let mut machine = Machine::new(compiled, 200, 200, vec![BASIC.clone(), SYS.clone()]).unwrap();
+    match machine.sync_run_all() {
+        Ok(State::Ok) => {},
+        Ok(reason) => {
+            println!("Machine: {:?}, state: {:?}", machine, reason);
+        },
+        Err(e) => {
+            println!("Machine: {:?}, err: {:?}", machine, e);
+        }
+    }
+}
+

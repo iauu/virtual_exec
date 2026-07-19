19a31b551b071bbf08afc30fcfde1b3baf53b0dc
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2026-07-19 15:48:45 +0100

    fix(std): Fix underflow and divide by 0

--- virtual_exec_std/src/func/pop_array.rs
+++ virtual_exec_std/src/func/pop_array.rs
@@ -9,7 +9,7 @@ use virtual_exec_type::ext::SafeReadArcExt;
 fn pop_array_sync<'a>(array: Collection<'a>) -> Result<Any<'a>, Error> {
     let capacity = array.read_arc_safe().capacity();
     let length = array.read_arc_safe().len();
-    if capacity - 100 > length || capacity / length > 2 {
+    if capacity.saturating_sub(100) > length || capacity / length.max(1) > 2 {
         array.write_arc_blocking().shrink_to_fit();
     }
     array.write_arc_blocking().pop().ok_or(ExecutionError::NonRecoverable(NonRecoverableError::IndexOutOfRangeError))
